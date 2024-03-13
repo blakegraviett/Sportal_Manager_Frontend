ce28bdf55aabc17c81a76659ee5385ad55794da8
Blake Graviett <blake.graviett@gmail.com>
2024-03-13 12:33:06 -0500

    fix body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    component: LandingPageComponent,
     pathMatch: 'full',
   },
   {
-    path: 'home',
-    component: LandingPageComponent,
+    path: 'auth',
+    component: AuthComponent,
   },
 ];
 
