aa9c52b935aee89c14c35a6c46da6883f64b6f3f
Blake Graviett <blake.graviett@gmail.com>
2024-03-15 11:06:05 -0500

    add watch game

--- src/app/events/event-card/event-card.component.ts
+++ src/app/events/event-card/event-card.component.ts
@@ -4,24 +4,35 @@ import { EventsService } from '../events.service';
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
-  styleUrl: './event-card.component.scss'
+  styleUrl: './event-card.component.scss',
 })
 export class EventCardComponent {
-  @Input() events
-  homeTeam
-  awayTeam
-  constructor(private eventsService: EventsService) { }
+  @Input() events;
+  homeTeam;
+  awayTeam;
+  constructor(private eventsService: EventsService) {}
 
   ngOnInit() {
-    this.getTeamNameAndIcon()
+    this.getTeamNameAndIcon();
   }
 
   getTeamNameAndIcon() {
-    this.eventsService.getSingleTeam(this.events.teams[0]).subscribe((response) => {
-      this.homeTeam = response.data
-    })
-    this.eventsService.getSingleTeam(this.events.teams[1]).subscribe((response) => {
-      this.awayTeam = response.data
-    })
+    this.eventsService
+      .getSingleTeam(this.events.teams[0])
+      .subscribe((response) => {
+        this.homeTeam = response.data;
+      });
+    this.eventsService
+      .getSingleTeam(this.events.teams[1])
+      .subscribe((response) => {
+        this.awayTeam = response.data;
+      });
+  }
+
+  getLivePeriodScore() {
+    const periodArr = this.events.period;
+    const mostRecentString = periodArr[periodArr.length - 1];
+
+    return mostRecentString;
   }
 }
