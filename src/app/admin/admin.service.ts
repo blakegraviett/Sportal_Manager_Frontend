96b310815390c30539c4ccc87cde8d1d2b29d319
Blake Graviett <blake.graviett@gmail.com>
2024-03-15 21:46:09 -0500

    working patch for events

---
+++ src/app/admin/admin.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  apiUrl: string = 'https://event-manager-03bk.onrender.com/api/v1/';
+  constructor(private http: HttpClient) {}
+
+  // update an event
+  updateEvent(id, name, description, awayTeam, homeTeam, link) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const credentials = { name, description, awayTeam, homeTeam, link };
+    const options = { headers, withCredentials: true };
+    return this.http.patch(`${this.apiUrl}events/${id}`, credentials, options);
+  }
+  // get all workers in an event
+  getAllWorkers() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers, withCredentials: true };
+    return this.http.get<any>(`${this.apiUrl}users`, options);
+  }
+  getAllEventWorker(userId) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers, withCredentials: true };
+    return this.http.get<any>(`${this.apiUrl}users/${userId}`, options);
+  }
+
+  getAllTeams() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers, withCredentials: true };
+    return this.http.get<any>(`${this.apiUrl}teams`, options);
+  }
+  getSingleTeam(id) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const options = { headers, withCredentials: true };
+    return this.http.get<any>(`${this.apiUrl}teams/${id}`, options);
+  }
+}
--- src/app/admin/manage-events/manage-events.component.ts
+++ src/app/admin/manage-events/manage-events.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { EventsService } from '../../events/events.service';
+import { AdminService } from '../admin.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-events',
@@ -7,8 +9,13 @@ import { EventsService } from '../../events/events.service';
   styleUrl: './manage-events.component.scss',
 })
 export class ManageEventsComponent {
+  // * DEFAULTS
   selectedOption: string = 'Select an option';
   isDropdownOpen: boolean = false;
+  allUsers: any = [''];
+  allEventWorkers: any = [''];
+  allEvents: Array<any>;
+  allTeams: any = [''];
   homeTeam = {
     name: '',
     logo: '',
@@ -24,25 +31,72 @@ export class ManageEventsComponent {
     startTime: '',
     link: '',
     ticketLink: '',
+    teams: {
+      homeTeam: '',
+      awayTeam: '',
+    },
   };
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
 
+  // * LOGIC
+  // Once the dropdown is selected
   selectOption(event: any) {
     this.selectedOption = event.name;
     this.toggleDropdown();
     this.selectedEvent = event;
     this.getTeamNameAndIcon(event);
+
+    this.adminService.getAllTeams().subscribe(
+      (response) => {
+        this.allTeams = response.data;
+      },
+      (error) => {
+        console.error('Error:', error); // Handle error
+      }
+    );
+    this.adminService
+      .getSingleTeam(this.selectedEvent.teams.homeTeam)
+      .subscribe(
+        (response) => {
+          this.selectedEvent.teams.homeTeam = response.data.name;
+          console.log(':', this.selectedEvent.teams.homeTeam);
+        },
+        (error) => {
+          console.error('Error:', error); // Handle error
+        }
+      );
+    this.adminService
+      .getSingleTeam(this.selectedEvent.teams.awayTeam)
+      .subscribe(
+        (response) => {
+          this.selectedEvent.teams.awayTeam = response.data.name;
+        },
+        (error) => {
+          console.error('Error:', error); // Handle error
+        }
+      );
+    // get all avalible users
+    // this.adminService.getAllWorkers().subscribe(
+    //   (response) => {
+    //     console.log(response);
+    //     this.allUsers = response.data.users;
+    //   },
+    //   (error) => {
+    //     console.error('Error:', error); // Handle error
+    //   }
+    // );
   }
 
-  constructor(private eventService: EventsService) {}
-  allEvents: Array<any>;
+  constructor(
+    private eventService: EventsService,
+    private adminService: AdminService
+  ) {}
   ngOnInit() {
     this.eventService.getAllEvents().subscribe(
       (res) => {
         this.allEvents = res.data;
-        console.log('res.data:', res.data);
       },
       (error) => {
         console.error('Error fetching data:', error); // Handle error
@@ -51,10 +105,48 @@ export class ManageEventsComponent {
   }
 
   saveChanges() {
-    // Implement logic to save changes to event options
-    // console.log('Event Name:');
-    // console.log('Event Date:');
-    // Save changes to other event options
+    const updatedEvent = {
+      id: this.selectedEvent._id,
+      name: this.updateEvent.value.name,
+      description: this.updateEvent.value.description,
+      link: this.updateEvent.value.link,
+      awayTeam: this.updateEvent.value.awayTeam,
+      homeTeam: this.updateEvent.value.homeTeam,
+    };
+    console.log('this.selectedEvent.name:', this.selectedEvent.name);
+
+    if (updatedEvent.name === '') {
+      updatedEvent.name = this.selectedEvent.name;
+    }
+    if (updatedEvent.description === '') {
+      updatedEvent.description = this.selectedEvent.description;
+    }
+    if (updatedEvent.link === '') {
+      updatedEvent.link = this.selectedEvent.link;
+    }
+    if (updatedEvent.awayTeam === '') {
+      updatedEvent.awayTeam = this.selectedEvent.teams.awayTeam;
+    }
+    if (updatedEvent.homeTeam === '') {
+      updatedEvent.homeTeam = this.selectedEvent.teams.homeTeam;
+    }
+    this.adminService
+      .updateEvent(
+        updatedEvent.id,
+        updatedEvent.name,
+        updatedEvent.description,
+        updatedEvent.awayTeam,
+        updatedEvent.homeTeam,
+        updatedEvent.link
+      )
+      .subscribe(
+        (response) => {
+          location.reload();
+        },
+        (error) => {
+          console.error('Error:', error); // Handle error
+        }
+      );
   }
 
   getTeamNameAndIcon(events) {
@@ -69,4 +161,14 @@ export class ManageEventsComponent {
         this.awayTeam = response.data;
       });
   }
+
+  updateEvent = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    link: new FormControl(''),
+    homeTeam: new FormControl(''),
+    awayTeam: new FormControl(''),
+    date: new FormControl(''),
+    startTime: new FormControl(''),
+  });
 }
